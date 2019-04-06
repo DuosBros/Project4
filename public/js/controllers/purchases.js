bdfc94debdf7c216912afce4effd2807508064bb
Tomas Mocek <tomas.mocek@teliasonera.com>
2019-04-06 15:16:03 +0200

    manual addition of purchases

--- public/js/controllers/purchases.js
+++ public/js/controllers/purchases.js
@@ -3,17 +3,12 @@
 
 var myApp = angular.module('medPharmaController');
 
-myApp.controller('PurchasesCtrl', ['$scope', 'medPharmaOthers', 'medPharmaPurchases',
-	function($scope, medPharmaOthers, medPharmaPurchases) {
+myApp.controller('PurchasesCtrl', ['$scope', 'medPharmaOthers', 'medPharmaPurchases', '$modal',
+	function($scope, medPharmaOthers, medPharmaPurchases, $modal) {
 		$scope.isMobile = medPharmaOthers.isMobile();
 
-		$scope.loading = true;
-		medPharmaPurchases.getPurchases()
-		.then(function(purchases) {
-			$scope.purchases = purchases.data;
-			console.log(purchases.data);
-			$scope.loading = false;
-		});
+		getAllPurchases();
+		$scope.loggedInUser = medPharmaOthers.getLoggedInUser()
 
 		$scope.printProducts = function(products) {
 			var str = '';
@@ -30,4 +25,64 @@ myApp.controller('PurchasesCtrl', ['$scope', 'medPharmaOthers', 'medPharmaPurcha
 			return str;
 		}
 
+		medPharmaOthers.getAllProductsJson()
+        .then(function(products) {
+            $scope.allProductNames = Object.keys(products);
+        });
+
+		$scope.openAddModal = function() {
+			var $modalScope = $scope.$new(true);
+			$modalScope.purchase = {
+				products: [{
+                    productName: "",
+                    count: 1
+                }],
+				to: 'hlasenska@medpharma.cz',
+				user: $scope.loggedInUser,
+			};
+			$modalScope.allProductNames = $scope.allProductNames;
+
+			var modal = $modal({
+							scope: $modalScope,
+							templateUrl: 'partials/modals/purchasesForm.html',
+							show: false
+							});
+			modal.$promise.then(modal.show);
+
+			$modalScope.addPurchase = function() {
+				medPharmaPurchases.addPurchase($modalScope.purchase)
+				.then(function() {
+					getAllPurchases();
+					modal.$promise.then(modal.hide);
+				});
+			}
+
+			$modalScope.removeProduct = function(index) {
+                $modalScope.purchase.products.splice(index, 1);
+			}
+
+			$modalScope.addProduct = function() {
+                var lastProduct = $modalScope.purchase.products[$modalScope.purchase.products.length - 1];
+                if (lastProduct.productName !== "") {
+                    $modalScope.purchase.products.push({
+                        productName: "",
+                        count: 1
+                    });
+                }
+            };
+
+			$modalScope.close = function() {
+				this.$hide();
+			}
+		}
+
+		function getAllPurchases() {
+			$scope.loading = true;
+			medPharmaPurchases.getPurchases()
+			.then(function(purchases) {
+				$scope.purchases = purchases.data;
+				console.log(purchases.data);
+				$scope.loading = false;
+			});
+		}
 }]);
--- scripts/copyToLocal2.sh
+++ scripts/copyToLocal2.sh
@@ -5,6 +5,7 @@ mongoexport --host ds153890.mlab.com:53890 -u medpharma2 -p TranMedGroup12e -d h
 mongoexport --host ds153890.mlab.com:53890 -u medpharma2 -p TranMedGroup12e -d heroku_gvlqrgxg --collection warehouse --out temp/warehouse.json
 mongoexport --host ds153890.mlab.com:53890 -u medpharma2 -p TranMedGroup12e -d heroku_gvlqrgxg --collection products --out temp/products.json
 mongoexport --host ds153890.mlab.com:53890 -u medpharma2 -p TranMedGroup12e -d heroku_gvlqrgxg --collection users --out temp/users.json
+mongoexport --host ds153890.mlab.com:53890 -u medpharma2 -p TranMedGroup12e -d heroku_gvlqrgxg --collection purchases --out temp/purchases.json
 
 
 mongoimport --drop --db medpharma --collection orders --file temp/orders.json
@@ -13,3 +14,4 @@ mongoimport --drop --db medpharma --collection senders --file temp/senders.json
 mongoimport --drop --db medpharma --collection warehouse --file temp/warehouse.json
 mongoimport --drop --db medpharma --collection products --file temp/products.json
 mongoimport --drop --db medpharma --collection users --file temp/users.json
+mongoimport --drop --db medpharma --collection purchases --file temp/purchases.json
