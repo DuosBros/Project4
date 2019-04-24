2a68c1e6ad8c1bfece0d23a9b896ea0604af0e67
An Tran <tnmephagroup@gmail.com>
2019-04-24 20:12:44 +0200

    lint

--- handlers/warehouse.js
+++ handlers/warehouse.js
@@ -1,5 +1,3 @@
-
-
 var Q = require('q');
 var mongo;
 var ACTIVE_ORDERS_STATE;
@@ -8,7 +6,7 @@ var warehouseHandler;
 var ProdHandler = require('../handlers/products.js').Handler;
 var prodHandler;
 
-Handler = function(app) {
+Handler = function (app) {
     mongo = app.get('mongodb');
     ACTIVE_ORDERS_STATE = app.get('ACTIVE_ORDERS_STATE');
     warehouseHandler = this;
@@ -16,13 +14,13 @@ Handler = function(app) {
     prodHandler = new ProdHandler(app);
 };
 
-Handler.prototype.saveProductAmount = function(productName, amount, calculationDate, difference, user, notificationThreshold) {
+Handler.prototype.saveProductAmount = function (productName, amount, calculationDate, difference, user, notificationThreshold) {
     var deferred = Q.defer();
 
     var productsV2 = mongo.collection('productsV2');
 
     productsV2.update(
-        {'name': productName},
+        { 'name': productName },
         {
             $set: {
                 'warehouse.amount': amount,
@@ -37,27 +35,27 @@ Handler.prototype.saveProductAmount = function(productName, amount, calculationD
                 }
             }
         },
-        function(err, result) {
-        if(result.result.n == 1) {
-            deferred.resolve(result);
-        } else {
-            var error = new Error('error while updating product ' + productName);
-            console.log(error + '> ' + err);
-            error.status = 400;
-            deferred.reject(error);
-        }
-    });
+        function (err, result) {
+            if (result.result.n == 1) {
+                deferred.resolve(result);
+            } else {
+                var error = new Error('error while updating product ' + productName);
+                console.log(error + '> ' + err);
+                error.status = 400;
+                deferred.reject(error);
+            }
+        });
 
     return deferred.promise;
 }
 
-Handler.prototype.getSingleProductData = function(productName) {
+Handler.prototype.getSingleProductData = function (productName) {
     var deferred = Q.defer();
 
     var productsV2 = mongo.collection('productsV2');
 
-    productsV2.findOne({name: productName}, function(err, result) {
-        if(err) {
+    productsV2.findOne({ name: productName }, function (err, result) {
+        if (err) {
             deferred.reject(err);
         } else {
             deferred.resolve(result.warehouse);
@@ -66,13 +64,13 @@ Handler.prototype.getSingleProductData = function(productName) {
     return deferred.promise;
 }
 
-Handler.prototype.getProductsData = function() {
+Handler.prototype.getProductsData = function () {
     var deferred = Q.defer();
 
     var productsV2 = mongo.collection('productsV2');
 
-    productsV2.find().toArray(function(err, result) {
-        if(err) {
+    productsV2.find().toArray(function (err, result) {
+        if (err) {
             deferred.reject(err);
         } else {
             var tmpProductsData = [];
@@ -91,50 +89,50 @@ Handler.prototype.getProductsData = function() {
     return deferred.promise;
 }
 
-Handler.prototype.getProductsInOrders = function(productName) {
+Handler.prototype.getProductsInOrders = function (productName) {
     var deferred = Q.defer();
 
     var ordersCollection = mongo.collection('orders');
     var match;
-    var unwind = {$unwind: "$products"};
-    var projection = {$project: {'products.productName': 1, 'products.count': 1, 'paymentDate': { $ifNull: ['$payment.paymentDate', 'notPaid']}, '_id': 0}};
-    var projection2 = {$project: {'products.productName': 1, 'products.count': 1, 'paymentDate': {$cond: { if: { $eq: [ "$paymentDate", 'notPaid' ] }, then: 'notPaid', else: 'paid' }}, '_id': 0}};
-    var match2 = {$match: {'products.productName': productName}};
-    var group = {$group: {_id: {paymentDate: "$paymentDate", product: "$products.productName"},  count: {$sum: "$products.count"}}};
-    var projection3 = {$project: {'product': "$_id.product", 'payment': '$_id.paymentDate', 'count': 1, _id: 0}};
+    var unwind = { $unwind: "$products" };
+    var projection = { $project: { 'products.productName': 1, 'products.count': 1, 'paymentDate': { $ifNull: ['$payment.paymentDate', 'notPaid'] }, '_id': 0 } };
+    var projection2 = { $project: { 'products.productName': 1, 'products.count': 1, 'paymentDate': { $cond: { if: { $eq: ["$paymentDate", 'notPaid'] }, then: 'notPaid', else: 'paid' } }, '_id': 0 } };
+    var match2 = { $match: { 'products.productName': productName } };
+    var group = { $group: { _id: { paymentDate: "$paymentDate", product: "$products.productName" }, count: { $sum: "$products.count" } } };
+    var projection3 = { $project: { 'product': "$_id.product", 'payment': '$_id.paymentDate', 'count': 1, _id: 0 } };
     var pipeline = [];
 
     this.getProductsCalculationDate(productName)
-    .then(function(calculationDate) {
-        match = {$match: {'state': ACTIVE_ORDERS_STATE, 'payment.orderDate' : {'$gte': calculationDate}}};
+        .then(function (calculationDate) {
+            match = { $match: { 'state': ACTIVE_ORDERS_STATE, 'payment.orderDate': { '$gte': calculationDate } } };
 
-        pipeline.push(match);
-        pipeline.push(projection);
-        pipeline.push(projection2);
-        pipeline.push(unwind);
-        pipeline.push(match2);
-        pipeline.push(group);
-        pipeline.push(projection3);
+            pipeline.push(match);
+            pipeline.push(projection);
+            pipeline.push(projection2);
+            pipeline.push(unwind);
+            pipeline.push(match2);
+            pipeline.push(group);
+            pipeline.push(projection3);
 
-        ordersCollection.aggregate(pipeline, function(err, result) {
-            if(err) {
-                deferred.reject(err);
-            } else {
-                deferred.resolve(warehouseHandler.mapResults(productName, result));
-            }
-        });
-    })
+            ordersCollection.aggregate(pipeline, function (err, result) {
+                if (err) {
+                    deferred.reject(err);
+                } else {
+                    deferred.resolve(warehouseHandler.mapResults(productName, result));
+                }
+            });
+        })
 
     return deferred.promise;
 }
 
-Handler.prototype.mapResults = function(productName, results) {
+Handler.prototype.mapResults = function (productName, results) {
     var resultObject = {};
     resultObject[productName] = {};
     resultObject[productName].paid = 0;
     resultObject[productName].notPaid = 0;
-    results.forEach(function(result) {
-        if(result.payment == "paid") {
+    results.forEach(function (result) {
+        if (result.payment == "paid") {
             resultObject[productName].paid = result.count;
         } else {
             resultObject[productName].notPaid = result.count;
@@ -143,15 +141,15 @@ Handler.prototype.mapResults = function(productName, results) {
     return resultObject;
 }
 
-Handler.prototype.getProductsCalculationDate = function(productName) {
+Handler.prototype.getProductsCalculationDate = function (productName) {
     var deferred = Q.defer();
 
     var productsV2 = mongo.collection('productsV2');
 
 
-    var match = {'name' : productName};
-    productsV2.findOne(match, function(err, result) {
-        if(err) {
+    var match = { 'name': productName };
+    productsV2.findOne(match, function (err, result) {
+        if (err) {
             deferred.reject(err);
         } else if (!result) {
             deferred.resolve(new Date());
@@ -169,7 +167,7 @@ function mapWarehouseV2(whData) {
         products: [],
     };
 
-    Object.keys(whData.products).forEach(function(key) {
+    Object.keys(whData.products).forEach(function (key) {
         var product = whData.products[key];
         product.name = key;
 
@@ -197,7 +195,7 @@ function calculateProductInput(warehouse, month, year) {
     return 0;
 }
 
-Handler.prototype.getWarehouseV2 = function(year, month) {
+Handler.prototype.getWarehouseV2 = function (year, month) {
     var deferred = Q.defer();
 
     var data = {
@@ -208,10 +206,9 @@ Handler.prototype.getWarehouseV2 = function(year, month) {
     };
 
     prodHandler.getAllProductsJson()
-    .then(function(products) {
-        console.log(products)
-        Object.keys(products).forEach(function(key) {
-                if(products[key].category === "Nonbillable") {
+        .then(function (products) {
+            Object.keys(products).forEach(function (key) {
+                if (products[key].category === "Nonbillable") {
                     delete products[key]
                 }
                 else {
@@ -219,19 +216,19 @@ Handler.prototype.getWarehouseV2 = function(year, month) {
                     product.input = calculateProductInput(product.warehouse, month, year);
                     delete product.warehouse;
                 }
+            });
+
+            data.products = products;
+
+            deferred.resolve(mapWarehouseV2(data));
+        })
+        .fail(function (err) {
+            console.log('ERR getting warehouse V2: ' + err);
+            deferred.reject(err);
         });
-
-        data.products = products;
-
-        deferred.resolve(mapWarehouseV2(data));
-    })
-    .fail(function(err) {
-        console.log('ERR getting warehouse V2: ' + err);
-        deferred.reject(err);
-    });
 
     return deferred.promise;
 }
 
-
 exports.Handler = Handler;
+
